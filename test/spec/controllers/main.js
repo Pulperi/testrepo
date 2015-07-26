e6f048ff1d9485779d8ab25e50fe1eae175050cb
pulperi <pulperi@users.noreply.github.com>
2015-07-26 20:47:51 +0300

    first release

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 describe('Controller: MainCtrl', function () {
@@ -16,7 +17,19 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('list should be empty at start', function () {
+    expect(scope.todos.length).toBe(0);
+  });
+
+  it('should add items to list', function() {
+    scope.todo = 'Test2';
+    scope.addTodo();
+    expect(scope.todos.length).toBe(1);
+  });
+
+  it('should remove items from list', function() {
+    scope.todos = ['Test1', 'Test2'];
+    scope.removeTodo(0);
+    expect(scope.todos.length).toBe(1);
   });
 });
